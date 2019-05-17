84ae6281f687beace6f43e7e302dd55d49da8167
roniaxe <roniaxe@gmail.com>
2019-05-17 17:59:51 -0400

    Added missing files

---
+++ WoodForestConversion.API/Conversion/DTOs/ArchonStepDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoodForestConversion.Data;
+
+namespace WoodForestConversion.API.Conversion.DTOs
+{
+    public class ArchonStepDto
+    {
+        public string ArchonStepName { get; set; }
+        public string ArchonConfiguration { get; set; }
+        public Guid ParentTaskID { get; set; }
+        public string DisplayTitle { get; set; }
+        public ExecutionModule ExecutionModule { get; set; }
+    }
+}
